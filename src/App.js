ea8eedb75c4d1662dfb80c9db98e39fd1d65bf31
hanakrasniiqi <hk69945@ubt-uni.net>
2025-05-05 00:00:23 +0200

    Path fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
+import ScrollToTop from './ScrollToTop';
 import SignUp from './pages/SignUp'; 
 import LogIn from './pages/LogIn';
 
